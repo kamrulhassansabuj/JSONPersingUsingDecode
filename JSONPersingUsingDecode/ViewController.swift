9338ab26436d02e1094e0bf9dfe2aeafda647aec
Md. Kamrul Hassan Sabuj <sabujsahi@macs-mac-mini.local>
2018-10-22 10:57:44 +0600

    use Decodable , url session fro JSN parsing

--- JSONPersingUsingDecode/ViewController.swift
+++ JSONPersingUsingDecode/ViewController.swift
@@ -10,9 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    struct Course: Decodable {
+        let id : Int
+        let name : String
+        let link : String
+        let imageURL : String
+        
+        
+        init(json: [String: Any]) {
+            id = json["id"] as? Int ?? 0
+            name = json["name"] as? String ?? ""
+            link = json["link"] as? String ?? ""
+            imageURL = json["imageURL"] as? String ?? ""
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let jsonURLString = "https://api.letsbuildthatapp.com/jsondecodable/course"
+        
+        guard let url = URL(string: jsonURLString) else {
+            return
+        }
+        URLSession.shared.dataTask(with: url) { (data, url, error) in
+            // check error
+            // check response status 200 ok
+            guard let data = data else {return}
+            
+//            let dataAsString = String.init(data: data, encoding: .utf8)
+//
+//            print(dataAsString )
+            do {
+                //Swift 4 or later
+                let course = try JSONDecoder().decode(Course.self, from: data)
+                print(course.name)
+                //Swift 2/3
+//                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:Any] else {return}
+//
+//                let course = Course(json: json)
+//                print(course.name)
+            }catch{
+                print("Error in Decoding : \(error)")
+            }
+            
+        }.resume()
+//        let myCourse = Course(id: 1, name: "My Name", link: "Some Link", imageURL: "Some URL")
     }
 
 
